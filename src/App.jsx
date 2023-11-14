75deba26050f95b85fda57197795c3374f4f9167
Sebastian Gallego <sebastian.gallego@gmail.com>
2023-11-13 23:27:02 -0300

    inicio del Todo-List

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>To-Do-List</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/componentes/ListaTareas.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ListaTareas() {
+  return <div>ListaTareas</div>;
+}
