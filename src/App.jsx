a52740f09ce0fde9dda87fd30591da2b3a1b9678
Sebastian Gallego <sebastian.gallego@gmail.com>
2023-11-15 08:49:05 -0300

    nextUi + TailwindCCS

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,58 @@
+import confetti from "canvas-confetti";
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  Image,
+  Button,
+  User,
+} from "@nextui-org/react";
+
 function App() {
   return (
     <>
-      <h1>To-Do-List</h1>
+      <div className="bg-slate-800  h-screen w-screen flex flex-col items-center gap-5 ">
+        <Button
+          className="bg-fuchsia-500 hover:bg-fuchsia-900 w-[150px]"
+          onClick={() => confetti()}
+          color=""
+          variant="light"
+        >
+          Confetti
+        </Button>
+
+        <User
+          name="Jane Doe"
+          description="Product Designer"
+          avatarProps={{
+            src: "https://i.pravatar.cc/150?u=a04258114e29026702d",
+          }}
+          className="w-[250px] h-[50px] bg-orange-500 font-bold"
+        />
+
+        <div className="px-6 container mx-auto ">...</div>
+
+        <Card
+          shadow="sm"
+          isPressable
+          onPress={() => console.log("item pressed")}
+        >
+          <CardBody className="overflow-visible p-0">
+            <Image
+              shadow="sm"
+              radius="lg"
+              width="100%"
+              alt={"Orange"}
+              className="w-full object-cover h-[140px]"
+              src={"/images/fruit-1.jpeg"}
+            />
+          </CardBody>
+          <CardFooter className="text-small justify-between">
+            <b>{"Orange"}</b>
+            <p className="text-default-500">{"$5.50"}</p>
+          </CardFooter>
+        </Card>
+      </div>
     </>
   );
 }
--- src/componentes/ListaTareas.jsx
+++ src/componentes/ListaTareas.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function ListaTareas() {
   return <div>ListaTareas</div>;
 }
